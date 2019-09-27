11bbcf0c6b560b1a4c7c457e4a974234cf212c4c
kajiguchi97 <yuiluna20wolf@gmail.com>
2019-09-27 19:14:34 +0900

    delete program

--- program/0926/abc.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-    int a = 10;
-    int b = 100;
-    int c = a * b;
-    printf("%d * %d = %d", a, b, c);
-    return 0;
-}
--- program/1212/mymodule1.py
+++
@@ -1,6 +0,0 @@
-
-# mymodule1.py
-def plus(a, b):
-    return a + b
-def multiply(a, b):
-    return a * b
